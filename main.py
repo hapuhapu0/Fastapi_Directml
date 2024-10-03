e1ca227362b49c70f19108b773f0a0e2737e070e
hapuhapu0 <tmdtn3624@naver.com>
2024-10-03 18:22:59 +0900

    저장

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
-from typing import Annotated
-import multiprocessing
-import uvicorn
+from typing import Annotated, Union
 from fastapi import FastAPI, Header, Body
 from pydantic import BaseModel
 
+from tendo import singleton
+
+import multiprocessing
+import uvicorn
 import onnxruntime as ort
 import numpy as np
 import cv2
 import base64
 
+import win32gui
+import win32ui
+import win32con
+
+singleton.SingleInstance()
+
 class DetectItem(BaseModel):
-    base64Image: str
-    Tscore: str | None = 0.8
+    x1: int
+    y1: int
+    x2: int
+    y2: int
+    Tscore: int | None = 0.8
 
 class DetectImage:
     def __init__(self) -> None:
@@ -20,52 +31,120 @@ class DetectImage:
 
         # Error message
         # Model not loaded
-        self.Error_modelNotLoad = "Model not Loaded"
+        self.error_ModelNotLoad = "Model not Loaded"
 
-app = FastAPI()
-Detect = DetectImage()
+async def captureScreen(Pos: tuple, GrayScale: bool = True) -> np.ndarray:
+    try:
+        width = Pos[2] - Pos[0]
+        height = Pos[3] - Pos[1]
 
-def preprocess(img: base64.b64encode) -> np.ndarray:
-    # load image
-    decodeImage = base64.b64decode(img)
-    nparr = np.frombuffer(decodeImage, np.uint8)
-    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        hdesktop = win32gui.GetDesktopWindow()
+        desktop_dc = win32gui.GetWindowDC(hdesktop)
+        dcObj = win32ui.CreateDCFromHandle(desktop_dc)
+        cDC = dcObj.CreateCompatibleDC()
+        dataBitMap = win32ui.CreateBitmap()
 
+        dataBitMap.CreateCompatibleBitmap(dcObj, width, height)
+        cDC.SelectObject(dataBitMap)
+        cDC.BitBlt((0, 0), (width, height), dcObj, (Pos[0], Pos[1]), win32con.SRCCOPY)
+
+        bmpinfo = dataBitMap.GetInfo()
+        bmpstr = dataBitMap.GetBitmapBits(True)
+        img = np.frombuffer(bmpstr, dtype='uint8')
+        img.shape = (height, width, 4)
+
+        if (GrayScale == True):
+            img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        return img
+    
+    except Exception as e:
+        return False
+    
+    finally:
+        dcObj.DeleteDC()
+        cDC.DeleteDC()
+        win32gui.DeleteObject(dataBitMap.GetHandle())
+
+async def addPadding(Image: np.ndarray) -> np.ndarray:
+    height, width = Image.shape[:2]
+
+    margin = [np.abs(height - width) // 2, np.abs(height - width) // 2]
+
+    if (np.abs(height - width) % 2 != 0):
+        margin[0] += 1
+
+    if (height < width):
+        margin_list = [margin, [0, 0]]
+    else:
+        margin_list = [[0, 0], margin]
+
+    if len(Image.shape) == 3:
+        margin_list.append([0,0])
+
+    output = np.pad(Image, margin_list, mode='constant')
+    return output
+    
+async def preProcess(image: base64.b64encode) -> np.ndarray:
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = await addPadding(image)
     image = cv2.resize(image, dsize=(640, 640))
     image_data = np.array(image) / 255.0
     image_data = np.transpose(image_data, (2, 0, 1))
     image_data = np.expand_dims(image_data, axis=0).astype(np.float32)
     return image_data
-    
 
-@app.get("/detect")
-async def detectImage(item: DetectItem):
+async def postProcess(result: list, Tscore: float, shape: list) -> np.ndarray:
+    obj = result[0][0]
+    x1, y1, x2, y2, scores = obj
+    
+    if len(scores) > 0:
+        max_score = np.max(scores)
+    else:
+        max_score = 0
+    
+    if max_score > Tscore:
+        detected_objects = []
+        
+        scale_x = shape[1] / 640
+        scale_y = shape[0] / 640
+        
+        for i in range(len(scores)):
+            if scores[i] > Tscore:
+                scaled_x = round(x1[i] * scale_x)
+                scaled_y = round(y1[i] * scale_y)
+                
+                detected_objects.append([scaled_x, scaled_y])
+        
+        return detected_objects
+    
+    return []
+
+app = FastAPI()
+Detect = DetectImage()
+
+@app.post("/detect")
+async def detectImage(
+    capturePos: Annotated[list | None, Header()],
+    Tscore: Annotated[str | None, Header()] = 0.8
+):
     try:
-        base64Image = item.base64Image
-        Tscore = item.Tscore
-        print(Tscore)
+        capturePos = eval(capturePos[0])
         if (Detect.trainModel == None):
-            return Detect.Error_modelNotLoad
+            return Detect.error_ModelNotLoad
         else:
             Tscore = float(Tscore)
-            imageData = preprocess(base64Image)
+            ScreenImage = await captureScreen(capturePos, False)
+            w, h, _ = ScreenImage.shape
 
-            output = Detect.trainModel.run(None, {"images": imageData})
-            obj = output[0][0]
+            ScreenImage = await preProcess(ScreenImage)
 
-            x1, y1, x2, y2, scores = obj
-            if len(scores) > 0:
-                max_score = np.max(scores)
-            else:
-                max_score = 0
-            if max_score > Tscore:
-                detected_objects = []
-                for i in range(len(scores)):
-                    if scores[i] > Tscore:
-                        detected_objects.append([round(x1[i]), round(y1[i])])
-                return detected_objects
+            output = Detect.trainModel.run(None, {"images": ScreenImage})
+            result = await postProcess(output, Tscore, [w, h])
+            
+            return result
     except Exception as e:
-        return e
+        print(f"Unknown Error error: {e}")
+        return f"Unknown Error error: {e}"
 
 @app.get("/setmodel")
 async def setmodel(model: Annotated[str | None, Header()], Provider: Annotated[str | None, Header()] = None):
--- test.ahk
+++ test.ahk
@@ -1,52 +1,49 @@
-#include CreateFormData.ahk
+﻿model := "best.onnx"
+capturePos := "[0, 0, 1000, 1000]"
+Tscore := 0.8
 
-FileRead, Read, Image.txt
-JsonData := ({"base64Image": Read, "Tscore": "0.8"})
-CreateFormData(PostData, ContentType, oForm)
+Find := new Detect()
 
-wh := ComObjCreate("WinHTTP.WinHTTPRequest.5.1")
-wh.open("post", "http://127.0.0.1:8000/detect")
-wh.SetRequestHeader("Content-Type", "application/json")
-wh.send(PostData)
-msgbox, % wh.ResponseText()
+F1::
+if Find.setModel(model) {
+    a := A_TickCount
+    result := Find.requestDetect(capturePos, Tscore)
+    msgbox, % "결과값: " result "`n 소요시간: " A_TickCount - a
+}
+Else
+    msgbox, % 모델 로드 실패
 
-; FileRead, Read, Image.txt
+Return
+    
 
-; oForm := { id: myid1
-;           , index: 0
-;           , file: ["ClipboardImage.png"] }
-; CreateFormData(PostData, ContentType, oForm)
-
-; Find := new Detect()
-; if Find.setModel("best.onnx")
-;     Find.requestDetect(Read, 0.7)
-; Else
-;     msgbox, % 모델 로드 실패
-exitapp
-exit
+F5::
+ExitApp
+Exit
 
 class Detect {
-    __New() {
+    __New(Path := "main.exe", SleepTime := 5000) {
+        run, % Path,,Hide,PID
+        sleep, % SleepTime
         this.wh := ComObjCreate("WinHTTP.WinHTTPRequest.5.1")
     }
 
     setModel(modelPath) {
-        this.wh.open("get", "http://127.0.0.1:8000/setmodel")
+        this.wh.open("GET", "http://127.0.0.1:8000/setmodel")
         this.wh.SetRequestHeader("model", modelPath)
         this.wh.send()
         return this.wh.ResponseText()
     }
 
     getProvider() {
-        this.wh.open("get", "http://127.0.0.1:8000/getProviders")
+        this.wh.open("GET", "http://127.0.0.1:8000/GETProviders")
         this.wh.send()
         return this.wh.ResponseText()
     }
 
-    requestDetect(base64Image, Score) {
-        this.wh.open("get", "http://127.0.0.1:8000/detect")
-        this.wh.SetRequestHeader("base64Image", base64Image)
-        this.wh.SetRequestHeader("Tscore", Score)
+    requestDetect(capturePos, Tscore) {
+        this.wh.open("POST", "http://127.0.0.1:8000/detect")
+        this.wh.SetRequestHeader("capturePos", capturePos)
+        this.wh.SetRequestHeader("Tscore", Tscore)
         this.wh.send()
         return this.wh.ResponseText()
     }
